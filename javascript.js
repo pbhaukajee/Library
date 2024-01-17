5d8b60fb1f67ae6d16408660a7e144c987cee9c4
Priyanka <pbhaukajee@gmail.com>
2024-01-17 11:57:59 -0600

    display book to the table

--- javascript.js
+++ javascript.js
@@ -1,22 +1,32 @@
 const myLibrary = [];
+const tableData = document.querySelector("tbody");
 
-function Book(title, author, pages, Read) {
+function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
-  this.Read = Read;
+  this.read = read;
 }
 
-function addBookToLibrary() {
-  const title = "Alchemist";
-  const author = "Paulo Coehlo";
-  const pages = 200;
-  const Read = "yes";
-
-  const newBook = new Book(title, author, pages, Read);
+function addBookToLibrary(title, author, pages, read) {
+  const newBook = new Book(title, author, pages, read);
   myLibrary.push(newBook);
 }
 
-addBookToLibrary();
+function displayBook() {
+  myLibrary.forEach((book) => {
+    let tableRow = `<tr class="table-data"><td>${book.title}</td>
+    <td>${book.author}</td>
+    <td>${book.pages}</td>
+    <td class="read">${book.read}</td><td><button class="delete">Delete</button></td></tr>`;
+    tableData.insertAdjacentHTML("beforeend", tableRow);
+  });
+}
+
+addBookToLibrary("Alchemist", "Paulo Coehlo", 345, "read");
+addBookToLibrary("Rich Dad Poor Dad", "Robert", 280, "read");
+addBookToLibrary("You're a Bad Ass", "Jen Sinero", 200, "read");
+
+displayBook();
 
 console.log(myLibrary);
